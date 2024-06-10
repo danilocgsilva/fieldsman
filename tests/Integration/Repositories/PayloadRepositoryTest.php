5174c3bbb78d0f1dd7f0c0266673d06a7f11ce55
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-06-10 10:39:36 +0000

    wip still creating first test

--- tests/Integration/Repositories/PayloadRepositoryTest.php
+++ tests/Integration/Repositories/PayloadRepositoryTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Danilocgsilva\Fieldsman\Tests\Integration\Repositories;
 
+use Danilocgsilva\Fieldsman\Entities\PayloadEntity;
 use PHPUnit\Framework\TestCase;
 use Danilocgsilva\Fieldsman\Repositories\PayloadRepository;
 use PDO;
@@ -23,7 +24,30 @@ class PayloadRepositoryTest extends TestCase
     
     public function test1Store(): void
     {
+        $this->assertSame(0, $this->countPayloads());
+        
         $payloadRepository = new PayloadRepository($this->pdo);
-        $this->assertTrue(false);
+
+        $payloadName = "2024-05-01-120000";
+        $payloadContent = <<<EOF
+{
+    "name": "John Doe",
+    "age": "22",
+    "occupy": "develo;er",
+    "nationality": "Germany"
+}
+EOF;
+        
+        $payload = new PayloadEntity($payloadName, $payloadContent);
+    }
+
+    private function countPayloads(): int
+    {
+        $queryCount = "SELECT COUNT(*) FROM payloads;";
+        $preResults = $this->pdo->prepare($queryCount);
+        $preResults->execute();
+        $preResults->setFetchMode(PDO::FETCH_NUM);
+        $row = $preResults->fetch();
+        return $row[0];
     }
 }
